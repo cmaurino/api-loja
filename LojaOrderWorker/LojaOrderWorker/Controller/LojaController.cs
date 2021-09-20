0f8313d98b3d350fba097267376328e7fa213bd5
Caio Maurino <caio.maurino@hotmail.com>
2021-09-20 19:05:38 -0300

    Add files via upload

---
+++ LojaOrderWorker/LojaOrderWorker/Controller/LojaController.cs
@@ -0,0 +1,48 @@
+﻿using EstoqueAPI.Models;
+using EstoqueAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaOrderWorker.Controllers
+{
+    public class LojaController : ControllerBase
+    {
+        private readonly EstoqueService _estoqueService;
+
+        public LojaController(EstoqueService estoqueService)
+        {
+            _estoqueService = estoqueService;
+        }
+        public ActionResult<Produto> Get(string id)
+        {
+            var Produto = _estoqueService.Get(id);
+
+            if (Produto == null)
+            {
+                return NotFound();
+            }
+
+            return Produto;
+        }
+        public IActionResult Update(string id, Produto ProdutoIn)
+        {
+            var Produto = _estoqueService.Get(id);
+
+            if (Produto == null)
+            {
+                return NotFound();
+            }
+
+            Produto.Id = null;
+
+            _ = _estoqueService.UpdateAsync(id, ProdutoIn);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ LojaOrderWorker/LojaOrderWorker/Domain/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaOrderWorker.Domain
+{
+    public sealed class Order
+    {
+        public int NumeroPedido { get; set; }
+        public string IdProduto { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
